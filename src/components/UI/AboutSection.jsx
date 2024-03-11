a89aa088722162b17a6825d1cc941c3e3865a695
Savindya Bandara <savindyabandara413@gmail.com>
2024-03-11 14:58:29 +0530

    first commit

---
+++ src/components/UI/AboutSection.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import { Container, Row, Col } from "reactstrap";
+import "../../styles/about-section.css";
+import aboutImg from "../../assets/all-images/cars-img/bmw-offer.png";
+
+const AboutSection = ({ aboutClass }) => {
+  return (
+    <section
+      className="about__section"
+      style={
+        aboutClass === "aboutPage"
+          ? { marginTop: "0px" }
+          : { marginTop: "280px" }
+      }
+    >
+      <Container>
+        <Row>
+          <Col lg="6" md="6">
+            <div className="about__section-content">
+              <h4 className="section__subtitle">About Us</h4>
+              <h2 className="section__title">Welcome to car rent service</h2>
+              <p className="section__description">
+                At Otto Car Sale, customer satisfaction is our top priority. Our team of friendly and knowledgeable staff is dedicated to providing personalized service and assistance every step of the way. From helping you choose the right vehicle for your needs to offering tips for your journey, we're here to ensure that your rental experience exceeds your expectations.
+
+                Whether you're exploring a new city, embarking on a weekend getaway, or simply need a temporary replacement vehicle, trust Otto Car Sale to provide reliable transportation solutions tailored to your needs. Welcome to a world of convenience, reliability, and affordability – welcome to Otto Car Sale.
+              </p>
+
+              {/* <div className="about__section-item d-flex align-items-center">
+                <p className="section__description d-flex align-items-center gap-2">
+                  <i class="ri-checkbox-circle-line"></i> Lorem ipsum dolor sit
+                  amet.
+                </p>
+
+                <p className="section__description d-flex align-items-center gap-2">
+                  <i class="ri-checkbox-circle-line"></i> Lorem ipsum dolor sit
+                  amet.
+                </p>
+              </div>
+
+              <div className="about__section-item d-flex align-items-center">
+                <p className="section__description d-flex align-items-center gap-2">
+                  <i class="ri-checkbox-circle-line"></i> Lorem ipsum dolor sit
+                  amet.
+                </p>
+
+                <p className="section__description d-flex align-items-center gap-2">
+                  <i class="ri-checkbox-circle-line"></i> Lorem ipsum dolor sit
+                  amet.
+                </p>
+              </div> */}
+            </div>
+          </Col>
+
+          <Col lg="6" md="6">
+            <div className="about__img">
+              <img src={aboutImg} alt="" className="w-100" />
+            </div>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
+};
+
+export default AboutSection;
---
+++ src/pages/About.jsx
@@ -0,0 +1,73 @@
+import React from "react";
+
+import CommonSection from "../components/UI/CommonSection";
+import Helmet from "../components/Helmet/Helmet";
+import AboutSection from "../components/UI/AboutSection";
+import { Container, Row, Col } from "reactstrap";
+import BecomeDriverSection from "../components/UI/BecomeDriverSection";
+
+import driveImg from "../assets/all-images/drive.jpg";
+import "../styles/about.css";
+
+const About = () => {
+  return (
+    <Helmet title="About">
+      <CommonSection title="About Us" />
+      <AboutSection aboutClass="aboutPage" />
+
+      <section className="about__page-section">
+        <Container>
+          <Row>
+            <Col lg="6" md="6" sm="12">
+              <div className="about__page-img">
+                <img src={driveImg} alt="" className="w-100 rounded-3" />
+              </div>
+            </Col>
+
+            <Col lg="6" md="6" sm="12">
+              <div className="about__page-content">
+                <h2 className="section__title">
+                  We Are Committed To Provide Safe Ride Solutions
+                </h2>
+
+                <p className="section__description">
+                Welcome to Otto Car Sale, your premier destination for hassle-free car rentals! At Otto Car Sale, we understand that convenience, reliability, and affordability are paramount when it comes to renting a vehicle. That's why we strive to offer a seamless experience from booking to drop-off, ensuring that your journey is as smooth as possible.
+                </p>
+
+                <p className="section__description">
+                Our user-friendly website and mobile app make booking a breeze, allowing you to reserve your vehicle with just a few clicks. Plus, with flexible rental options and competitive rates, you can enjoy the freedom of the open road without breaking the bank.
+                </p>
+
+                <div className=" d-flex align-items-center gap-3 mt-4">
+                  <span className="fs-4">
+                    <i class="ri-phone-line"></i>
+                  </span>
+
+                  <div>
+                    <h6 className="section__subtitle">Need Any Help?</h6>
+                    <h4>+94770548789</h4>
+                  </div>
+                </div>
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+
+      <BecomeDriverSection />
+
+      <section>
+        <Container>
+          <Row>
+            <Col lg="12" className="mb-5 text-center">
+        
+            </Col>
+           
+          </Row>
+        </Container>
+      </section>
+    </Helmet>
+  );
+};
+
+export default About;
